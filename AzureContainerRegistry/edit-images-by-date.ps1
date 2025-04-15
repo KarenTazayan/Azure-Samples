2725ffc1dc4a3daffaae7d95b58a16d93c645817
Karen Tazayan <karen.tazayan@outlook.com>
2025-04-15 03:21:35 -0700

    Enhance `edit-images-by-date.ps1` script to use updated ACR commands and improve image modification logic; add `view-image-properties.azcli` for image property retrieval.

--- AzureContainerRegistry/edit-images-by-date.ps1
+++ AzureContainerRegistry/edit-images-by-date.ps1
@@ -1,7 +1,7 @@
 param
 (
-    [Parameter (Mandatory = $true)]
-    [DateTime] $DateTo
+  [Parameter (Mandatory = $true)]
+  [DateTime] $DateTo
 )
 
 $currentDate = Get-Date
@@ -19,10 +19,15 @@ $repositories = az acr repository list --name $acrName --output json | ConvertFr
 # Loop through each repository and fetch manifests with creation date
 $allImages = @()
 foreach ($repo in $repositories) {
-    # az acr repository show-manifests --name $acrName --repository $repo --output json 
-    $manifests = az acr repository show-manifests --name $acrName --repository $repo `
-      --query "[].{Repository: '$repo', Digest: digest, Timestamp: timestamp}" --output json | ConvertFrom-Json
-    $allImages += $manifests
+  #az acr repository show-manifests --name $acrName --repository $repo --output json
+  $manifests = az acr manifest list-metadata --name $repo --registry $acrName `
+    --query "[].{Repository: '$repo', Digest: digest, Timestamp: createdTime, Tags: tags}" --output json | ConvertFrom-Json
+    
+  # WARNING: This command has been deprecated and will be removed in a future release. Use 'acr manifest list-metadata' instead. 
+  # $manifests = az acr repository show-manifests --name $acrName --repository $repo `
+  #   --query "[].{Repository: '$repo', Digest: digest, Timestamp: timestamp}" --output json | ConvertFrom-Json
+    
+  $allImages += $manifests
 }
 
 # Display results
@@ -30,8 +35,16 @@ Write-Output "All Images Count: $($allImages.Count)"
 $oldImages = $allImages | Where-Object { [datetime]::Parse($_.Timestamp) -lt $dateThreshold }
 Write-Output "Old Images Count: $($oldImages.Count)"
 
-foreach($oldImage in $oldImages) {
-   az acr repository update --name $acrName --image "$($oldImage.Repository)@$($oldImage.Digest)" --write-enabled true `
-    --query ["createdTime, digest, tags"]
-   Write-Output "Modified: $($oldImage.Repository)@$($oldImage.Digest)"
+foreach ($oldImage in $oldImages) {
+  Write-Output $oldImage
+  if ($oldImage.Tags -and $oldImage.Tags.Count -gt 0) {
+    az acr repository update --name $acrName --image "$($oldImage.Repository):$($oldImage.Tags[0])" --write-enabled true `
+      --query ["createdTime, digest"]
+    Write-Output "Modified: $($oldImage.Repository):$($oldImage.Tags[0])"
+  }
+  else {
+    az acr repository update --name $acrName --image "$($oldImage.Repository)@$($oldImage.Digest)" --write-enabled true `
+      --query ["createdTime, digest"]
+    Write-Output "Modified: $($oldImage.Repository)@$($oldImage.Digest)"
+  }
 }
